4d00994aa420bb1a81f6aab55708b0229d74a806
Tobias Böker <tb@softwarepioniere.de>
2020-07-27 00:03:27 +0200

    ..

--- terraform/x1/tf/variables.tf
+++ terraform/x1/tf/variables.tf
@@ -4,15 +4,20 @@ variable "location" {
   description = "Specify a location see: az account list-locations -o table"
 }
 
+variable "resource_prefix" {
+  type = string
+  default = "tb1"
+}
+
 variable "rgname" {
   type        = string
-  default     = "terraform-group"
+  default     = "{var.resource_prefix}_rg_my1"
   description = "the resource group"
 }
 
 variable "acrname" {
   type        = string
-  default     = "tbacr"
+  default     = "{var.resource_prefix}myacr1"
   description = "the azure container registry name"
 }
 
