d93bae567c0a46a69b5a82b894bc39b19c6a09ca
Tobias Böker <tb@softwarepioniere.de>
2020-07-27 00:10:05 +0200

    ..

--- terraform/x1/tf/main.tf
+++ terraform/x1/tf/main.tf
@@ -3,13 +3,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.rgname
+  name     = "${var.resource_prefix}_${var.rgname}"
   location = var.location
   tags     = var.tags
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = var.acrname
+  name                     = "${var.resource_prefix}_${var.acrname}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   # sku                      = "Premium"
