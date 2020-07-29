b1f94c54d12e4b59269c36928f4501238b45ce36
Tobias Böker <tb@softwarepioniere.de>
2020-07-29 23:47:03 +0200

    ..

--- code1-infrastructure/terraform/shared/dev/main.tf
+++ code1-infrastructure/terraform/shared/dev/main.tf
@@ -1,7 +1,7 @@
-# provider "azurerm" { 
-#   version = "=2.20.0"
-#   features {}
-# }
+provider "azurerm" { 
+  version = "=2.20.0"
+  features {}
+}
 
 module "app-shared" {
   source = "github.com/tboeker/terraform-azure-app-shared"
