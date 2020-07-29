403ffd62cf502cceee932c5d9fcd71cf172211c9
Tobias Böker <tb@softwarepioniere.de>
2020-07-30 00:50:35 +0200

    ..

--- code1-infrastructure/terraform/env/dev/main.tf
+++ code1-infrastructure/terraform/env/dev/main.tf
@@ -10,3 +10,22 @@ module "shared" {
   appshort = "tbcode1"
   envshort = "dev"
 }
+
+
+
+module "eventstore" {
+  source = "../modules/env-eventstore"
+
+  appname = "tb-code1"
+  appshort = "tbcode1"
+  envshort = "dev"
+
+  yoda_password = var.EVENTSTORE_YODA_PASSWORD
+  
+  keyvault_id               = module.shared.keyvault_id
+  vnet_id                   = module.shared.vnet_id
+  vnet_name                 = module.shared.vnet_name
+  vnet_resource_group_name  = module.shared.vnet_resource_group_name
+
+  location      = module.shared.location
+}
